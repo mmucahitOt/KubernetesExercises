1f8b88703962527f9ef22c425253b5b7cfe894e9
mmucahitOt <m.mucahit0@gmail.com>
2025-08-30 12:57:46 +0300

    Exercise 1.1. Getting started

---
+++ log_output/scripts/kubernetes_deploy.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# !!! IMPORTANT !!!
+# the user should be logged in to Docker Hub before running this script
+# docker login
+
+# This script is used to deploy the log_output application to a Kubernetes cluster.
+# It is used to test the application in a Kubernetes environment.
+
+# Get the registry name from the command line arguments
+DOCKER_REGISTRY=$1
+
+echo "--------------------------------"
+echo "Docker Registry name: $DOCKER_REGISTRY"
+echo "--------------------------------"
+
+# Build the Docker image
+docker build -t log_output:latest .
+echo "Docker image built"
+
+# Tag the image for Docker Hub
+docker tag log_output:latest $DOCKER_REGISTRY/log_output:latest
+echo "Docker image tagged"
+
+# Push the Docker image to Docker Hub
+docker push $DOCKER_REGISTRY/log_output:latest
+echo "Docker image pushed to Docker Hub"
+
+EXISTING_CONTEXT=$(kubectl config get-contexts | grep "k3d-k3s-default")
+
+if [ -z "$EXISTING_CONTEXT" ]; then
+  echo "--------------------------------"
+  echo "There is no cluster"
+  echo "--------------------------------"
+  k3d cluster create k3s-default --agents 2
+  kubectl config use-context k3d-k3s-default
+  echo "--------------------------------"
+  echo "Cluster created and context switched to it"
+  echo "--------------------------------"
+else
+  echo "--------------------------------"
+  echo "There is a cluster"
+  echo "--------------------------------"
+  kubectl config use-context k3d-k3s-default
+  echo "--------------------------------"
+  echo "Context switched to cluster"
+  echo "--------------------------------"
+fi
+
+kubectl cluster-info
+
+k3d cluster start
+echo "--------------------------------"
+echo "Cluster started"
+echo "--------------------------------"
+
+kubectl create deployment log-output-deployment --image=$DOCKER_REGISTRY/log_output:latest
+
+echo "--------------------------------"
+echo "Deployments"
+kubectl get deployments
+
+echo "--------------------------------"
+echo "Pods"
+kubectl get pods
+
+echo "--------------------------------"
+echo "Waiting for pod to be ready..."
+kubectl wait --for=condition=ready pod -l app=log-output-deployment --timeout=60s
+
+POD_NAME=$(kubectl get pods | grep "log-output-deployment" | awk '{print $1}')
+
+echo "--------------------------------"
+echo "Pod name: $POD_NAME"
+echo "--------------------------------"
+echo "Logs"
+kubectl logs -f $POD_NAME --all-containers=true
+
+echo "--------------------------------"
+echo "Deployment complete"
+echo "--------------------------------"
+
---
+++ log_output/scripts/kubernetes_undeploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script is used to undeploy the log_output application from a Kubernetes cluster.
+# It is used to clean up the environment after testing.
+
+# Get the registry name from the command line arguments
+DOCKER_REGISTRY=$1
+
+echo "--------------------------------"
+echo "Docker Registry name: $DOCKER_REGISTRY"
+echo "--------------------------------"
+
+
+EXISTING_CONTEXT=$(kubectl config get-contexts | grep "k3d-k3s-default")
+
+if [ -z "$EXISTING_CONTEXT" ]; then
+  echo "--------------------------------"
+  echo "There is no cluster"
+  echo "--------------------------------"
+else
+  echo "--------------------------------"
+  echo "There is a cluster"
+  echo "--------------------------------"
+
+  kubectl config use-context k3d-k3s-default
+
+  echo "--------------------------------"
+  echo "Context switched to cluster"
+  echo "--------------------------------"
+
+  kubectl delete deployment log-output-deployment
+
+  echo "--------------------------------"
+  echo "Deployment deleted"
+  echo "--------------------------------"
+fi
