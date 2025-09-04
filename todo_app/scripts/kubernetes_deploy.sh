5da45936da6b19b0fc7d7050887963796ebb2cdd
mmucahitOt <m.mucahit0@gmail.com>
2025-09-04 16:49:22 +0300

    1.4. todo_app Declarative approach

--- todo_app/scripts/kubernetes_deploy.sh
+++ todo_app/scripts/kubernetes_deploy.sh
@@ -8,12 +8,12 @@
 # It is used to test the application in a Kubernetes environment.
 
 # Get the registry name from the command line arguments
-DOCKER_REGISTRY=$1
+_DOCKER_REGISTRY=$1
 
 PORT=$2
 
 echo "--------------------------------"
-echo "Docker Registry name: $DOCKER_REGISTRY"
+echo "Docker Registry name: $_DOCKER_REGISTRY"
 echo "Port: $PORT"
 echo "--------------------------------"
 
@@ -22,11 +22,11 @@ docker build -t todo_app:latest .
 echo "Docker image built"
 
 # Tag the image for Docker Hub
-docker tag todo_app:latest $DOCKER_REGISTRY/todo_app:latest
+docker tag todo_app:latest $_DOCKER_REGISTRY/todo_app:latest
 echo "Docker image tagged"
 
 # Push the Docker image to Docker Hub
-docker push $DOCKER_REGISTRY/todo_app:latest
+docker push $_DOCKER_REGISTRY/todo_app:latest
 echo "Docker image pushed to Docker Hub"
 
 EXISTING_CONTEXT=$(kubectl config get-contexts | grep "k3d-k3s-default")
@@ -57,8 +57,13 @@ echo "--------------------------------"
 echo "Cluster started"
 echo "--------------------------------"
 
-# !! PORT argument is not working
-kubectl create deployment todo-app-deployment --image=$DOCKER_REGISTRY/todo_app:latest --port=$PORT
+# Export variables for substitution in manifest
+export DOCKER_REGISTRY=$_DOCKER_REGISTRY
+export PORT=$PORT
+
+# TODO: add port!!
+# Apply the Kubernetes manifest with substituted variables
+envsubst < manifests/deployment.yaml | kubectl apply -f -
 
 echo "--------------------------------" 
 echo "Deployments"
